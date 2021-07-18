830b6fb33f574e1d794e3e161a98a314b9a9277a
DESKTOP-CDSJHME\fakaf <fakafaka555@gmail.com>
2021-07-18 22:22:05 +0300

    aloha

---
+++ src/test/java/PureSelenide.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class PureSelenide {
+
+    String REPOSITORY = "eroshenkoam/allure-example";
+
+    @Test
+    public void repositorieIssueTest(){
+        open("https://github.com/");
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(REPOSITORY);
+        $(".header-search-input").submit();
+
+        $(By.linkText(REPOSITORY)).click();
+        $(partialLinkText("Issues")).click();
+        $(withText("#68")).should(Condition.exist);
+    }
+}
